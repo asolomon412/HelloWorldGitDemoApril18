dae97759c7095532ccb505d34d15839278f18652
Antonella Solomon <antonellasolomon@gmail.com>
2018-04-24 11:02:49 -0400

    first commit

---
+++ src/HelloWorldApp.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class HelloWorldApp {
+
+	public static void main(String[] args) {
+
+		// Single line comments
+		// More comments 
+		
+		/*
+		 * Block comments
+		 */
+		System.out.print("Hello World");
+		System.out.println("Hello World");
+		
+		Scanner scan = new Scanner(System.in);
+		
+		
+		
+	}
+
+}
